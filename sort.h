815142576843ade3a8f5eff74af96e71e1abf855
TendoVera <rutsvovera@gmail.com>
2024-03-05 14:46:20 -0800

    edits

--- sort.h
+++ sort.h
@@ -16,6 +16,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void counting_sort(int *array, size_t size);
+int get_max(int *array, int size)
 /* Printing helper functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
